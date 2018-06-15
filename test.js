639589c4866ffc8df85e55784210bbf4298c2640
und3rdg <und3rdg@gmail.com>
2018-06-15 10:32:18 +0100

    mv config to json, catch 404, catch "no orig", :Tabularize

--- test.js
+++ test.js
@@ -1,44 +1,12 @@
 // jshint esversion:6, node:true, strict:false, asi: true,
 
-const compare = require('resemblejs').compare;
-const chalk = require('chalk')
-const fs = require('fs')
-const puppeteer = require('puppeteer')
-const rimraf = require('rimraf');
+const puppeteer   = require('puppeteer')            // making screenshots
+const compare     = require('resemblejs').compare   // compare images
+const chalk       = require('chalk')                // colors in console.log
+const fs          = require('fs')                   // filesytem
+const rimraf      = require('rimraf')               // rm -rf
 
 let Dw = {
-  settings:{
-    first_run: false,
-    diff_mode: true,
-    timeout: 10000,
-    misMatch_tolerance: 5,
-
-    url:{
-      domain: 'http://domain.tld',
-      pages:[ 
-        '/en'
-      ]
-    },
-
-    width:{
-      small: 320,
-      medium: 750,
-      large: 1000,
-      xlarge: 1400
-    },
-
-    folder:{
-      root: 'screenshot/',
-      out: 'out/',
-      orig: 'orig/',
-      diff: 'diff/'
-    },
-    user_agent:{
-      mobile: "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36",
-      desktop: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
-    }
-  },
-
   delay: function(ms){
     return new Promise(resolve => setTimeout(resolve, ms))
   },
@@ -53,25 +21,33 @@ let Dw = {
 
     await page.setExtraHTTPHeaders({ 'User-Agent': user_agent })
     await page.setViewport({
-      width: width,
-      height: 500
+      width  : width,
+      height : 500
     })
-    await page.goto(this.settings.url.domain+url)
+    try {
+      await page.goto(this.settings.url.domain+url,{domcontentloaded: true})
+    } catch(err) {
+      console.log(chalk.red(this.settings.url.domain+url+" (404)"))
+    }
     await this.delay(this.settings.timeout)
     await page.screenshot({
-      path: folder + '/' + filename,
-      fullPage: true
+      path     : folder + '/' + filename,
+      fullPage : true
       // clip: { x: 0, y: 0, width: width, height: height }
     })
-    await this.get_diff(filename)
+    try{
+      await this.get_diff(filename)
+    } catch(err){
+      console.log(chalk.red("orig folder is empty."), chalk.yellow("Noting to compare."))
+    }
     await browser.close()
   },
 
   get_diff: function(filename){
-    const options = {};
-    const orig = this.settings.folder.orig + filename
-    const out = this.settings.folder.out + filename
-    const diff = this.settings.folder.diff + filename
+    const options = {}
+    const orig    = this.settings.folder.orig + filename
+    const out     = this.settings.folder.out + filename
+    const diff    = this.settings.folder.diff + filename
 
     compare(orig, out, options, function (err, data) {
       if (err || data.misMatchPercentage > this.settings.misMatch_tolerance ) {
@@ -81,11 +57,10 @@ let Dw = {
         })
       } else {
         console.log(chalk.green('All good! --> ') + filename + ' ' + chalk.green(data.misMatchPercentage + '%'))
-        // console.log(data.getBuffer())
       }
     }.bind(this))
     if(this.diff_mode){
-console.log(chalk.yellow(this.diff_mode))
+      console.log(chalk.yellow(this.diff_mode))
     }
   },
 
@@ -106,18 +81,17 @@ console.log(chalk.yellow(this.diff_mode))
   },
 
   site_url: async function(url){
-    await this.make_screenshoot(url, 'mob',  this.settings.width.small, this.settings.folder.out)
-    await this.make_screenshoot(url, 'mob',  this.settings.width.medium, this.settings.folder.out)
-    await this.make_screenshoot(url, 'desk', this.settings.width.large, this.settings.folder.out)
-    await this.make_screenshoot(url, 'desk', this.settings.width.xlarge, this.settings.folder.out)
+    await this.make_screenshoot(url , 'mob'  , this.settings.width.small  , this.settings.folder.out)
+    await this.make_screenshoot(url , 'mob'  , this.settings.width.medium , this.settings.folder.out)
+    await this.make_screenshoot(url , 'desk' , this.settings.width.large  , this.settings.folder.out)
+    await this.make_screenshoot(url , 'desk' , this.settings.width.xlarge , this.settings.folder.out)
   },
   init: function(){
-    process.setMaxListeners(0);
+    process.setMaxListeners(0)
+    this.settings = require( "./config.json" ) // local config, copy from sample-config.json
     console.log(chalk.yellow('start'))
     this.prerun()
     this.setup()
-    let folder = this.settings.folder.out
-    // this.make_screenshoot(this.settings.url.pages[0], false, 1000, folder)
     
     this.settings.url.pages.forEach( (el) => this.site_url(el) )
   }
